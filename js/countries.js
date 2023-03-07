18ca5e329b76a90ee4882576cba475ade7ef3383
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-03-07 14:09:05 -0300

    first commit

---
+++ js/countries.js
@@ -0,0 +1,17 @@
+export const countries = [
+  {
+    name: "Brasil",
+    picture: "./images/brazil.png",
+    ref: "brazil",
+  },
+  {
+    name: "Canadá",
+    picture: "./images/brazil.png",
+    ref: "canada",
+  },
+  {
+    name: "Austrália",
+    picture: "./images/brazil.png",
+    ref: "australia",
+  },
+]
