ea254702b4efc27274769c9170139433100502db
pedroviniciusn <pedrovinicius.dev@gmail.com>
2023-03-07 21:36:45 -0300

    feat: Pagina retornando dados referentes ao brasil

---
+++ js/brazil.js
@@ -0,0 +1,48 @@
+const Main = {
+  init: function () {
+    this.cacheSelectors();
+    this.getData();
+  },
+
+  cacheSelectors: function () {
+    this.containerContent = document.querySelector(".container_content");
+    this.allDeaths = document.querySelector("#all_deaths");
+    this.allCases = document.querySelector("#all_cases");
+  },
+
+  dataHtml: function (data) {
+    return `
+    <div class="state">
+      <span><strong>Estados:</strong> ${data.ProvinciaEstado.toLocaleString('pt-BR')} </span>
+      <span><strong>Confirmados:</strong> ${data.Confirmados.toLocaleString('pt-BR')}</span>
+      <span><strong>Mortes:</strong> ${data.Mortos.toLocaleString('pt-BR')}</span>
+    </div>
+    `;
+  },
+
+  getData: async function () {
+    let html = "";
+    let totalCases = 0;
+    let totalDeaths = 0;
+
+    let data = await fetch(
+      "https://dev.kidopilabs.com.br/exercicio/covid.php?pais=brazil"
+    ).then((response) => {
+      return response.json();
+    });
+
+    let arr = Object.entries(data);
+
+    arr.map((item) => {
+      totalCases += item[1].Confirmados;
+      totalDeaths += item[1].Mortos;
+      html += this.dataHtml(item[1]);
+    });
+    
+    this.allDeaths.innerHTML += totalDeaths.toLocaleString('pt-BR');
+    this.allCases.innerHTML += totalCases.toLocaleString('pt-BR');
+    this.containerContent.innerHTML = html;
+  },
+};
+
+Main.init();
--- js/main.js
+++ js/main.js
@@ -1,5 +1,52 @@
-import { countries } from './countries';
-
 const Main = {
-  
+
+  countries: [
+    {
+      name: "Brasil",
+      picture: "./images/brazil.png",
+      ref: "brazil",
+    },
+    {
+      name: "Canadá",
+      picture: "./images/canada.png",
+      ref: "canada",
+    },
+    {
+      name: "Austrália",
+      picture: "./images/australia.png",
+      ref: "australia",
+    },
+  ],
+
+  init: function() {
+    this.cacheSelectors();
+    this.getCountries();
+  },
+
+  cacheSelectors: function() {
+    this.content = document.querySelector('.container_content');
+  },
+
+  countryHtml: function(data) {
+    return `
+      <a href="${data.ref}.html">
+        <div class="country">
+          <img src="${data.picture}" alt="${data.ref}" />
+          <h2>${data.name}</h2>
+        </div>     
+      </a>
+    `
+  },
+
+  getCountries: function() {
+    let html = "";
+
+    this.countries.map((item) => {
+      html += this.countryHtml(item);
+    });
+
+    this.content.innerHTML = html;
+  }
 }
+
+Main.init();
